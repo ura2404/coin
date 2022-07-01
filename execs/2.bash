8628c0483aa56719a18641da5aaf1dc0a58ea182
ura2404 <ura@itx.ru>
2022-07-01 23:04:18 +0300

    .

--- execs/2.bash
+++
@@ -1,8 +0,0 @@
-EMAIL=u2rostov@gmail.com
-COIN=sero
-WALLET=2CdAhAar9nWECZqebKNS9b9JJYAoSbFaywocy8TaogAuYYPghidYWQ2J9FBcpXrrsD6bN14nS7dCvYLCPYV9XizVdEd1zpe55cZFLHzRYxmxkurjqhBufbt6hfZRjzWH7gCx
-IP=0.0.0.0
-PORT=4067
-DEVICES=0,1,2,3
-
-./t-rex -a progpow --coin sero -o stratum+tcp://pool2.sero.cash:8808 -u $WALLET -p x -w $NAME --api-bind-http $IP:$PORT -d $DEVICES
