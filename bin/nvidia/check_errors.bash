32c3d4d68db2261b30f51080c8ccf6f577a58cb0
ura2404 <ura@itx.ru>
2022-07-05 12:25:16 +0300

    .

--- bin/nvidia/check_errors.bash
+++ bin/nvidia/check_errors.bash
@@ -12,16 +12,24 @@ ERR1='Reboot the system to recover this GPU'
 find $ROOT/log -name 'nvidia_err.log' -mtime +10 -delete
 
 #
+N=`cat $ROOT/conf/nvidia.json | $JQ '.total' | sed 's/\"//g'`
+COUNT=`$SMI --format=csv --query-gpu=index | sed '1d' | wc -l`
+
 RET=`$SMI --format=csv --query-gpu=index`
 
 E1=`echo $RET | grep "$ERR1"`
 
 if [ -n "$E1" ]
 then
-    echo 'Make reboot by nvidia-smi errors'
-    echo `date +%Y-%m-%d_%H:%M:%S` 'Reboot by error "$E1"' >> $LOG
+    echo `date +%Y-%m-%d_%H:%M:%S` 'Reboot by error "'$E1'"' | tee -a $LOG
     sudo reboot
 else
-    echo 'Errors not found' >> /dev/null
+    if [ "$N" != "$COUNT" ]
+    then
+        echo `date +%Y-%m-%d_%H:%M:%S` 'Reboot by wrong GPU count ('$COUNT')' | tee -a $LOG
+        sudo reboot
+    else
+        echo 'Errors not found' >> /dev/null
+    fi
 fi
 
--- errors.bash
+++ errors.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 export ROOT=`pwd`
+export JQ=`which jq`
 export SMI=`which nvidia-smi`
 
 bash bin/nvidia/check_errors.bash
